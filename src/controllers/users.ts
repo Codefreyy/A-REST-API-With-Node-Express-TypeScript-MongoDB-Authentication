ca2431f458e7d35eab9aed0ae6635872135eb233
Yujie <yp25@st-andrews.ac.uk>
2024-04-11 18:36:38 +0100

    add delete user route and isOwner authentication middleware

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 
-import { getUsers } from '../db/users';
+import { deleteUserById, getUsers } from '../db/users';
+
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers()
         return res.json(users)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400)
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params
+        await deleteUserById(id)
+        return res.sendStatus(204)
     } catch (error) {
         console.log(error)
         return res.sendStatus(400)
